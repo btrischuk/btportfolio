e3c8209840dd9197c8777b7d5e51cd033a0ae32b
btrischuk <b-trischuk@hotmail.com>
2018-02-18 18:14:15 -0800

    Create front page and change proxy port.

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,7 @@ gulp.task('browser-sync', function() {
   ];
 
   browserSync.init(files, {
-    proxy: 'localhost8888/btportfolio'
+    proxy: 'localhost:8888/btportfolio'
   });
 
   gulp.watch(files).on('change', browserSync.reload);
