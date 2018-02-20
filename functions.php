59aa764e7c6b7f7ec3855468e7ae1f8ae4f5ab9b
btrischuk <b-trischuk@hotmail.com>
2018-02-19 20:17:25 -0800

    Style header,  add panda image and set it to home url.

--- functions.php
+++ functions.php
@@ -86,6 +86,9 @@ function btportfolio_scripts() {
 	wp_enqueue_style( 'btportfolio-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'btportfolio-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
+	
+	wp_enqueue_script( 'jquery');
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
