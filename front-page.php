1413f0e5c1796fb2b52627ac2fb8fbcc8681a8f8
btrischuk <b-trischuk@hotmail.com>
2018-02-23 00:43:22 -0800

    Style project page.

--- front-page.php
+++ front-page.php
@@ -42,35 +42,8 @@ get_header(); ?>
 
 		<?php endif; ?>
 
-		<section id="project" class="project-grid-container">
-    	<h2 class="project-header">My Projects</h2>
-    	<div class="latest-adventures-content">
-
-				<div class="inhabitent">
-					<h3 class="men">Inhabtent</h3>
-					<button>more details</button>
-				</div>
-
-				<div class="lovesea"> 
-					<h3 class="men">Lovesea</h3>
-					<button>more details</button>
-				</div>
-
-				<div class="aloha"> 
-					<h3 class="men">Aloha</h3>
-					<button>more details</button>
-				</div>
-
-				<div class="perfect-paddles">
-					<h3 class="men">Perfect Paddles</h3>
-					<button>more details</button>
-				</div>
-				
-			</div>
-    </section>
+	
   
-
-		</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
