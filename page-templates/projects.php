17145a0850dad2858b87e2dc6b3111dc947f290e
btrischuk <b-trischuk@hotmail.com>
2018-02-25 18:12:59 -0800

    Style and create links to project sites.

--- page-templates/projects.php
+++ page-templates/projects.php
@@ -11,63 +11,37 @@ get_header(); ?>
     <h2 class= "project-title"><?php echo CFS()->get( 'project_title' );	?></h2>
     <p class= "project-description"><?php echo CFS()->get( 'project_description' );	?></p>
     <img class= "project-image" src= '<?php echo CFS()->get( 'project_image' );	?>'>
-    <button>More Info</button>
+    <a class="project-buton" href="https://github.com/redacademy/lovesea-fall-2017/tree/master/themes/lovesea">Explore Site!</a></p>
   </div>
           
   <div class="entry-header ">
     <h2 class= "project-title"><?php echo CFS()->get( 'project_title2' );	?></h2>
     <p class= "project-description"><?php echo CFS()->get( 'project_description2' );	?></p>
     <img class= "project-image" src= '<?php echo CFS()->get( 'project_image2' );	?>'>
-    <button>More Info</button> 
+    <a class="project-buton" href="https://github.com/btrischuk/inhabitent">Explore Site!</a></p>
   </div>
           
   <div class="entry-header ">
     <h2 class= "project-title"><?php echo CFS()->get( 'project_title3' );	?></h2>
     <p class= "project-description"><?php echo CFS()->get( 'project_description3' );	?></p>
     <img class= "project-image" src= '<?php echo CFS()->get( 'project_image3' );	?>'>
-    <button>More Info</button>
+    <a class="project-buton" href="https://github.com/btrischuk/Quotes-on-Dev">Explore Site!</a></p>
   </div>
   
   <div class="entry-header ">
     <h2 class= "project-title"><?php echo CFS()->get( 'project_title4' );	?></h2>
     <p class= "project-description"><?php echo CFS()->get( 'project_description4' );	?></p>
     <img class= "project-image" src= '<?php echo CFS()->get( 'project_image4' );	?>'>
-    <button>More Info</button>
+    <a class="project-buton" href="https://btrischuk.github.io/pong/">Play Pong!</a></p>
   </div>
   
   <div class="entry-header ">
     <h2 class= "project-title"><?php echo CFS()->get( 'project_title5' );	?></h2>
     <p class= "project-description"><?php echo CFS()->get( 'project_description5' );	?></p>
     <img class= "project-image" src= '<?php echo CFS()->get( 'project_image5' );	?>'>
-    <button>More Info</button>
+    <a class="project-buton" href="https://btrischuk.github.io/instanews/">Explore Site!</a></p>
 	</div>
-
-	<section id="project" class="project-grid-container">
-    	<h2 class="project-header">My Projects</h2>
-    	<div class="latest-adventures-content">
-
-				<div class="inhabitent">
-					<h3 class="men">Inhabtent</h3>
-					<button>more details</button>
-				</div>
-
-				<div class="lovesea"> 
-					<h3 class="men">Lovesea</h3>
-					<button>more details</button>
-				</div>
-
-				<div class="aloha"> 
-					<h3 class="men">Aloha</h3>
-					<button>more details</button>
-				</div>
-
-				<div class="perfect-paddles">
-					<h3 class="men">Perfect Paddles</h3>
-					<button>more details</button>
-				</div>
 				
-			</div>
-    </section>
 
 </div>
 <?php get_footer(); ?>
