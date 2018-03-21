48d96e0cfd86aeedcc967f20cc51a1093258dd31
btrischuk <b-trischuk@hotmail.com>
2018-03-21 16:42:08 -0700

    Edit 404 page.

--- 404.php
+++ 404.php
@@ -3,6 +3,8 @@
  * The template for displaying 404 pages (not found).
  *
  * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ * 
+ *   * Template Name: error404
  *
  * @package btportfolio
  */
@@ -14,13 +16,11 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php echo esc_html( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
+					<h1 class="page-title"><?php echo esc_html( 'Uh-Oh! Wires must be crossed!' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
 					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?' ); ?></p>
-
-					<?php get_search_form(); ?>
 
 					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
@@ -28,23 +28,10 @@ get_header(); ?>
 					<div class="widget widget_categories">
 						<h2 class="widget-title"><?php echo esc_html( 'Most Used Categories' ); ?></h2>
 						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
+				
 						</ul>
 					</div><!-- .widget -->
 					<?php endif; ?>
-
-					<?php
-						$archive_content = '<p>' . sprintf( esc_html( 'Try looking in the monthly archives. %1$s' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
