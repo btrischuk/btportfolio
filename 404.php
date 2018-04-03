cd1fc8d88b1e86c8fc69ba838903cd6371650e00
btrischuk <b-trischuk@hotmail.com>
2018-04-03 15:09:27 -0700

    Style 404 page.

--- 404.php
+++ 404.php
@@ -20,9 +20,9 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?' ); ?></p>
+					<p><?php echo esc_html( 'Uh-Oh, Looks like you took a wrong turn somewhere...' ); ?></p>
+					<a href="<?php echo get_home_url(); ?>"> Click here to enter the portal to safety!</a>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
 					<?php if ( btportfolio_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
 					<div class="widget widget_categories">
